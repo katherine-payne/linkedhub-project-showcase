bb06164287658b68d37f74816f965183123b5da3
fraander <57777918+fraander@users.noreply.github.com>
2023-02-06 11:13:42 -0500

    added responsive design

--- src/user-profile.js
+++ src/user-profile.js
@@ -1,72 +1,19 @@
+import examplesFrank from "./examples";
 import ProjectCard from "./project-card";
 import ProjectCardSpotlight from "./project-card-spotlight";
 import UserProfileHeading from "./user-profile-heading";
 import UserProfileLabeledEntry from "./user-profile-labeled-entry";
 
 export default function UserProfile() {
-  const user = {
-    name: "Frank Anderson",
-    contact_info: {
-      email: "anderson.f@northeastern.edu",
-      phone: "(408) 805-9922",
-    },
-    experience: [
-      {
-        role: "Teaching Assistant",
-        company: "Northeastern University",
-        location: "Boston, MA",
-        start: "August 2022",
-        end: null,
-        description:
-          "Managed a team of 4 Teaching Assistants to instruct weekly labs teach review sessions for a section of 36 students.\nHeld weekly Office Hours to help students with challenging concepts and homework questions.",
-      },
-      {
-        role: "Specialist",
-        company: "Apple",
-        location: "Los Gatos, CA",
-        start: "June 2021",
-        end: "November 2021",
-        description:
-          "Communicated with customers to understand their needs, ignited their creative passions, and developed solutions unique to them.\nCollaborated with team members to close knowledge gaps and improve each other through specific and actionable feedback.",
-      },
-    ],
-    education: [
-      {
-        university: "Northeastern University",
-        degree: "Bachelor of Science",
-        major: "Computer Science",
-        gpa: "3.857",
-        start: "2021",
-        end: "2025",
-      },
-    ],
-    skills: [
-      "React",
-      "Tailwind",
-      "CSS",
-      "Bootstrap",
-      "Swift",
-      "Firebase",
-      "Scrum",
-      "Git",
-      "Python",
-    ],
-  };
+  const user = examplesFrank();
 
-  const projects = [{name: "Cool Project", hearts: 100}, {name: "My First Project", hearts: 1}];
-  const projectDescription = "This is my project!\nYou should check it out.\nIt's awesome!\n";
-  const languageTags = ["HTML", "CSS", "JavaScript", "Python"];
-  const topicTags = ["My Stuff", "Cool Projects", "Design", "Startup"];
-
-  function formatDescription (description) {
+  function formatDescription(description) {
     if (description.includes("\n")) {
       return (
         <ul className="list-disc ml-6">
-          {
-          description.split("\n").map( (bullet) => {
-            return (<li>{bullet}</li>);
-          })
-          }
+          {description.split("\n").map((bullet) => {
+            return <li>{bullet}</li>;
+          })}
         </ul>
       );
     } else {
@@ -75,11 +22,8 @@ export default function UserProfile() {
   }
 
   return (
-    <div className="grid grid-cols-3 gap-5">
-      <div
-        id="infoFeed"
-        className="font-serif  col-span-1 border-r-2 ml-4 p-4"
-      >
+    <div className="flex md:flex-row flex-col justify-center">
+      <div id="infoFeed" className="font-serif max-w-none md:max-w-lg border-r-2 ml-4 p-4">
         <p className="text-3xl font-semibold">{user.name}</p>
         <p>
           <a href={"mailto:" + user.contact_info.email}>
@@ -96,39 +40,62 @@ export default function UserProfile() {
         {user.experience.map((job) => {
           return (
             <div className="mb-4">
-
               <p className="font-bold">{job.role}</p>
 
               <UserProfileLabeledEntry label="Company" entry={job.company} />
               <UserProfileLabeledEntry label="Location" entry={job.location} />
-              <UserProfileLabeledEntry label="Duration" entry={job.start + " - " + (job.end ?? "Present")} />
-              <p className="whitespace-pre-wrap">{formatDescription(job.description)}</p>
+              <UserProfileLabeledEntry
+                label="Duration"
+                entry={job.start + " - " + (job.end ?? "Present")}
+              />
+              <p className="whitespace-pre-wrap">
+                {formatDescription(job.description)}
+              </p>
             </div>
           );
         })}
 
         <UserProfileHeading title={"Education"} />
         {user.education.map((edu) => {
-          return (<div className="mb-4">
-            <p className="font-bold">{edu.university}</p>
-            <UserProfileLabeledEntry label="Degree" entry={edu.degree} />
-            <UserProfileLabeledEntry label="Major" entry={edu.major} />
-            <UserProfileLabeledEntry label="Duration" entry={edu.start + " - " + edu.end} />
-            <UserProfileLabeledEntry label="Grade" entry={edu.gpa} />
-          </div>);
+          return (
+            <div className="mb-4">
+              <p className="font-bold">{edu.university}</p>
+              <UserProfileLabeledEntry label="Degree" entry={edu.degree} />
+              <UserProfileLabeledEntry label="Major" entry={edu.major} />
+              <UserProfileLabeledEntry
+                label="Duration"
+                entry={edu.start + " - " + edu.end}
+              />
+              <UserProfileLabeledEntry label="Grade" entry={edu.gpa} />
+            </div>
+          );
         })}
 
         <UserProfileHeading title={"Skills"} />
         <p className="mb-4">{user.skills.join(", ")}</p>
       </div>
 
-      <div id="projectFeed" className="col-span-2">
-        {projects.map((p, i) => (
-          <>
-            <ProjectCardSpotlight name={p.name} hearts={p.hearts} languageTags={languageTags} topicTags={topicTags} description={projectDescription}/>
-            <ProjectCard name={p.name} hearts={p.hearts} languageTags={languageTags} topicTags={topicTags} description={projectDescription}/>
-          </>
-        ))}
+      <div id="projectFeed" className="flex justify-center">
+        <div className="md:max-w-lg max-w-none">
+          {user.projects.map((p, i) => (
+            <>
+              <ProjectCardSpotlight
+                name={p.name}
+                hearts={p.hearts}
+                languageTags={p.languages}
+                topicTags={p.tags}
+                description={p.description}
+              />
+              <ProjectCard
+                name={p.name}
+                hearts={p.hearts}
+                languageTags={p.languages}
+                topicTags={p.tags}
+                description={p.description}
+              />
+            </>
+          ))}
+        </div>
       </div>
     </div>
   );
