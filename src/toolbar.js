b9106d7d27e9b1ac46dad8e84af5f2ef23bf0c49
fraander <57777918+fraander@users.noreply.github.com>
2023-02-08 13:37:35 -0500

    toolbar dropdown

--- src/toolbar.js
+++ src/toolbar.js
@@ -1,24 +1,33 @@
-import { FaSearch, FaPlus, FaHome } from "react-icons/fa";
+import {
+  FaSearch,
+  FaPlus,
+  FaHome,
+  FaPencilRuler,
+  FaDoorOpen,
+} from "react-icons/fa";
 
 export default function Toolbar() {
   return (
     <>
       <ul className="flex items-center justify-between mx-2 mt-2 mb-4 p-4 border-b-2">
         <li className="flex justify-between items-center">
-          <button className="flex items-center justify-center text-sm text-gray-500 hover:text-gray-700 border border-gray-30 hover:shadow hover:bg-gray-100 rounded-lg mr-4 p-2">
-            <span className="text-gray-500 hover:text-gray-700 p-2">
+          <button className="group transition-all flex items-center justify-center text-sm text-gray-500 hover:text-gray-700 border border-gray-30 hover:shadow hover:bg-gray-100 rounded-lg mr-4 p-2">
+            <span className=" transition-all text-gray-500 group-hover:text-gray-700 p-2">
               <FaHome />
             </span>
           </button>
 
-          <label for="search" className="group flex items-center justify-center text-sm text-gray-500 hover:text-gray-700 border border-gray-30 rounded-lg mr-8 p-2 cursor-text hover:bg-gray-100 hover:shadow">
+          <label
+            for="search"
+            className="transition-all group flex items-center justify-center text-sm text-gray-500 hover:text-gray-700 border border-gray-30 rounded-lg mr-8 p-2 cursor-text hover:bg-gray-100 hover:shadow"
+          >
             <label for="search" className="p-2 cursor-text ">
               <FaSearch />
             </label>
             <input
               type="search"
               id="search"
-              className="block w-full p-2 text-sm text-gray-900 bg-opacity-0 border rounded-lg outline-none border-none group-hover:bg-gray-100"
+              className=" transition-all block w-full p-2 text-sm text-gray-900 bg-opacity-0 border rounded-lg outline-none border-none group-hover:bg-gray-100"
               placeholder="Search"
               required
             />
@@ -26,19 +35,34 @@ export default function Toolbar() {
         </li>
 
         <li className="flex items-center justify-center">
-          <button className="flex items-center justify-center text-sm text-gray-500 hover:text-gray-700 border border-gray-30 hover:shadow hover:bg-gray-100 rounded-lg mr-8 p-2">
-            <span className="text-gray-500 hover:text-gray-700 p-2">
+          <button className="group transition-all flex items-center justify-center text-sm text-gray-500 hover:text-gray-700 border border-gray-30 hover:shadow hover:bg-gray-100 rounded-lg mr-8 p-2">
+            <span className=" transition-all text-gray-500 group-hover:text-gray-700 p-2">
               <FaPlus />
             </span>
             Add Project
           </button>
-          <button>
+          <div className="group">
+            <div className="absolute w-auto min-w-max right-4 scale-0 group-hover:scale-100 transition-all duration-100 bg-slate-100 rounded-lg mt-10 mr-10 shadow flex flex-col p-2 gap-2">
+              <button className="group transition-all flex items-center pr-6 text-sm text-gray-500 hover:text-gray-700 border border-gray-30 hover:shadow hover:bg-gray-100 rounded-lg p-2 bg-white">
+                <span className=" transition-all text-gray-500 group-hover:text-gray-700 p-2">
+                  <FaPencilRuler />
+                </span>
+                Edit Profile
+              </button>
+              <button className="group transition-all flex items-center pr-6 text-sm text-gray-500 hover:text-gray-700 border border-gray-30 hover:shadow hover:bg-gray-100 rounded-lg p-2 bg-white">
+                <span className=" transition-all text-gray-500 group-hover:text-gray-700 p-2">
+                  <FaDoorOpen />
+                </span>
+                Log Out
+              </button>
+            </div>
+
             <img
-              className="hover:drop-shadow-md w-16 h-16 rounded-full"
+              className="relative group-hover:drop-shadow-md w-16 h-16 rounded-full transition-all"
               src="https://picsum.photos/400"
               alt="Rounded avatar"
             />
-          </button>
+          </div>
         </li>
       </ul>
     </>
