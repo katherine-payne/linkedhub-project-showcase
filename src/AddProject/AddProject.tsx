738b9874a2dfa9a05f827e955e8bfc893d79457e
katherine-payne <kamapa001@gmail.com>
2023-03-02 23:46:36 -0500

    Remove commented code

--- src/AddProject/AddProject.tsx
+++ src/AddProject/AddProject.tsx
@@ -11,38 +11,6 @@ import { Repository } from "../Types/Repository";
 import { Language } from "../Types/Language";
 
 export default function AddProject() {
-  // const [title, setTitle] = useState("");
-  // const [tags, setTags] = useState(["Topic1", "Topic2"] as string[]);
-  // const [languages, setLanguages] = useState([
-  //   "Java",
-  //   "TypeScript",
-  // ] as string[]);
-
-  // return (
-  //   <div className="flex flex-col gap-2 mx-4 md:m-auto max-w-none md:max-w-xl">
-  //     <RepoSelector />
-  //     <div>
-  //       <label htmlFor="title" className="text-primary text-xl font-bold">
-  //         Title
-  //       </label>
-  //       <input
-  //         className="w-full block border border-border-neutral px-2 py-1.5 my-2"
-  //         id="title"
-  //         placeholder="Project Title"
-  //         value={title}
-  //         onChange={(e) => setTitle(e.target.value)}
-  //       />
-  //     </div>
-  //     <TagInput name="Tags" tagType="Topic" tags={tags} setTags={setTags} />
-  //     <TagInput
-  //       name="Languages"
-  //       tagType="Language"
-  //       tags={languages}
-  //       setTags={setLanguages}
-  //     />
-  //     <DescriptionInput />
-  //     <ImageSelector />
-  //   </div>
   const [link, setLink] = useState<string>("");
   const [repo, setRepo] = useState<Repository | null>(null);
   const [title, setTitle] = useState("");
@@ -56,18 +24,6 @@ export default function AddProject() {
     setTags(newRepo.topics);
     setLanguages(newRepo.languages.map((lang) => lang.name));
     setDescription(newRepo.description);
-  };
-
-  const formatLanguages = (languages: Array<Language>) => {
-    return languages
-      .map(
-        (lang) =>
-          lang.name +
-          " (" +
-          lang.lines.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") +
-          " lines)"
-      )
-      .join(", ");
   };
 
   return (
