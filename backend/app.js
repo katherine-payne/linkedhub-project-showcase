0d1e752e4663d7725bd941d7de734f4151b5cbc9
fraander <57777918+fraander@users.noreply.github.com>
2023-03-17 10:56:57 -0400

    added controllers

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,12 @@
 import express from "express"
+import CompanyController from "./controllers/comapny-controller"
+import ProjectController from "./controllers/project-controller"
+import UserController from "./controllers/user-controller"
 
 const app = express()
+
+CompanyController(app)
+ProjectController(app)
+UserController(app)
 
 app.listen(4000)
---
+++ backend/controllers/comapny-controller.js
@@ -0,0 +1,5 @@
+const CompanyController = (app) => {
+
+}
+
+export default CompanyController
