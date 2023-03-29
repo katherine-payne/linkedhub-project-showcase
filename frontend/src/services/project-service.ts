64aa1d5faea88bda23f34978ede04041f2977c13
katherine-payne <kamapa001@gmail.com>
2023-03-29 12:25:59 -0400

    Switch delete service methods to use id

--- frontend/src/services/project-service.ts
+++ frontend/src/services/project-service.ts
@@ -2,24 +2,24 @@ import axios from "axios";
 import Project from "src/Types/Project";
 import BASE_URL from "./service-helper";
 
-const PROJECTS_API = BASE_URL + "projects"
+const PROJECTS_API = BASE_URL + "projects";
 
 export const getProject = async (pid: string) => {
-   const response = await axios.get(PROJECTS_API + "/" + pid)
-   return response.data 
-}
+  const response = await axios.get(PROJECTS_API + "/" + pid);
+  return response.data;
+};
 
 export const addProject = async (project: Project) => {
-   const response = await axios.post(PROJECTS_API, project)
-   return response.data
-}
+  const response = await axios.post(PROJECTS_API, project);
+  return response.data;
+};
 
 export const updateProject = async (project: Project) => {
-   const response = await axios.put(PROJECTS_API + "/" + project._id, project)
-   return response.data
-}
+  const response = await axios.put(PROJECTS_API + "/" + project._id, project);
+  return response.data;
+};
 
-export const deleteProject = async (project: Project) => {
-   const response = await axios.delete(PROJECTS_API + "/" + project._id)
-   return response.data
-}
+export const deleteProject = async (pid: string) => {
+  const response = await axios.delete(PROJECTS_API + "/" + pid);
+  return response.data;
+};
