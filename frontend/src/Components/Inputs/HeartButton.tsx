57ee2277e3435e0970663825d5f5f35859164448
katherine-payne <kamapa001@gmail.com>
2023-04-17 18:20:14 -0400

    Forward anonymous users to login on heart click

--- frontend/src/Components/Inputs/HeartButton.tsx
+++ frontend/src/Components/Inputs/HeartButton.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router";
 import { FaHeart } from "react-icons/fa";
 import { AppDispatch, RootState } from "src/redux/store";
 import { updateProject } from "src/services/project-service";
@@ -8,18 +9,20 @@ import { profileThunk } from "src/services/user-thunks";
 import Project from "src/Types/Project";
 
 type Props = {
-  project : Project;
-  setProject : React.Dispatch<React.SetStateAction<Project>>
   hearted: boolean;
-  setHearted : React.Dispatch<React.SetStateAction<boolean>>
-}
+  setHearted: React.Dispatch<React.SetStateAction<boolean>>;
+  project: Project;
+  setProject: React.Dispatch<React.SetStateAction<Project>>;
+};
 
 export default function HeartButton({
+  hearted,
+  setHearted,
   project,
   setProject,
-  hearted,
 }: Props) {
   const dispatch = useDispatch<AppDispatch>();
+  const navigate = useNavigate();
   const currentUser = useSelector(
     (state: RootState) => state.users.currentUser
   );
@@ -29,29 +32,33 @@ export default function HeartButton({
     <button
       className={`flex 
         ${hearted ? "text-heart" : "text-neutral"} text-xl ml-auto font-mono`}
-      disabled={!heartEnabled}
       onClick={async (e) => {
         e.stopPropagation();
-        let updatedProject = { ...project };
-        if (hearted) {
-          updatedProject.hearts -= 1;
-        } else {
-          updatedProject.hearts += 1;
-        }
-        await updateProject(updatedProject);
-        if (updatedProject._id && currentUser) {
-          const updatedUser = { ...currentUser };
-          if (!hearted) {
-            updatedUser.liked = [...updatedUser.liked, updatedProject._id];
+        if (heartEnabled) {
+          let updatedProject = { ...project };
+          if (hearted) {
+            updatedProject.hearts -= 1;
           } else {
-            updatedUser.liked = updatedUser.liked.filter(
-              (id) => id !== updatedProject._id
-            );
+            updatedProject.hearts += 1;
           }
-          await updateUser(updatedUser);
-          await dispatch(profileThunk());
+          await updateProject(updatedProject);
+          if (updatedProject._id && currentUser) {
+            const updatedUser = { ...currentUser };
+            if (!hearted) {
+              updatedUser.liked = [...updatedUser.liked, updatedProject._id];
+            } else {
+              updatedUser.liked = updatedUser.liked.filter(
+                (id) => id !== updatedProject._id
+              );
+            }
+            await updateUser(updatedUser);
+            await dispatch(profileThunk());
+          }
+          setProject(updatedProject);
+          setHearted(!hearted);
+        } else {
+          navigate("/login");
         }
-        setProject(updatedProject);
       }}
     >
       <FaHeart className="mt-1 mr-1 hover:border hover:border-opacity-0 hover:border-slate-500" />
